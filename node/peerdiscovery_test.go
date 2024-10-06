77cca3ef876339ca163084d3d6f8d2da43186b01
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-10-06 17:03:27 +0200

    peerdisc: refactor the PeerDiscovery test

--- node/peerdiscovery_test.go
+++ node/peerdiscovery_test.go
@@ -12,26 +12,39 @@ import (
 	"google.golang.org/grpc"
 )
 
+func createPeerDiscovery(
+  ctx context.Context, wg *sync.WaitGroup, bootstrap, start bool,
+) *node.PeerDiscovery {
+  var peerDiscCfg node.PeerDiscoveryCfg
+  if bootstrap {
+    peerDiscCfg = node.PeerDiscoveryCfg{NodeAddr: bootAddr, Bootstrap: true}
+  } else {
+    peerDiscCfg = node.PeerDiscoveryCfg{NodeAddr: nodeAddr, SeedAddr: bootAddr}
+  }
+  peerDisc := node.NewPeerDiscovery(ctx, wg, peerDiscCfg)
+  if start {
+    wg.Add(1)
+    go peerDisc.DiscoverPeers(100 * time.Millisecond)
+  }
+  return peerDisc
+}
+
 func TestPeerDiscovery(t *testing.T) {
   ctx, cancel := context.WithCancel(context.Background())
   defer cancel()
   wg := new(sync.WaitGroup)
-  // Create peer discovery for the bootstrap node
-  peerDiscCfg := node.PeerDiscoveryCfg{NodeAddr: bootAddr, Bootstrap: true}
-  bootPeerDisc := node.NewPeerDiscovery(ctx, wg, peerDiscCfg)
+  // Create the peer discovery without staring for the bootstrap node
+  bootPeerDisc := createPeerDiscovery(ctx, wg, true, false)
   // Start the gRPC server on the bootstrap node
   grpcStartSvr(t, bootAddr, func(grpcSrv *grpc.Server) {
     node := rpc.NewNodeSrv(bootPeerDisc, nil)
     rpc.RegisterNodeServer(grpcSrv, node)
   })
-  // Create peer discovery for the new node
-  peerDiscCfg = node.PeerDiscoveryCfg{NodeAddr: nodeAddr, SeedAddr: bootAddr}
-  nodePeerDisc := node.NewPeerDiscovery(ctx, wg, peerDiscCfg)
-  // Start peer discover on the new node
-  wg.Add(1)
-  go nodePeerDisc.DiscoverPeers(100 * time.Millisecond)
+  // Create and start the peer discovery for the new node
+  nodePeerDisc := createPeerDiscovery(ctx, wg, false, true)
   // Wait for the peer discovery to discover peers
   time.Sleep(150 * time.Millisecond)
+  // Verify that the bootstrap node and the new node have discovered each other
   if !slices.Contains(bootPeerDisc.Peers(), nodeAddr) {
     t.Errorf("node address %v is not in bootstrap known peers", nodeAddr)
   }
