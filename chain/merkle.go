fd007e8253072c1c9c5eca74b87f99696778ac2f
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-11-05 12:02:28 +0100

    merkle: start MerleProve

--- chain/merkle.go
+++ chain/merkle.go
@@ -3,34 +3,79 @@ package chain
 import (
 	"fmt"
 	"math"
+	"slices"
 	"strconv"
 )
+
+func pairHash(l, r int) int {
+  if r != 0 {
+    hash, _ := strconv.Atoi(fmt.Sprintf("%d%d", l, r))
+    return hash
+  }
+  return l
+}
 
 func MerkleHash(txs []int) ([]int, error) {
   if len(txs) == 0 {
     return nil, fmt.Errorf("merkle hash: empty transaction list")
   }
-  halfFloor := func(v int) int {
-    return int(math.Floor(float64(v / 2)))
+  halfFloor := func(i int) int {
+    return int(math.Floor(float64(i / 2)))
   }
   l := int(math.Pow(2, math.Ceil(math.Log2(float64(len(txs)))) + 1) - 1)
-  mt := make([]int, l)
+  merkleTree := make([]int, l)
   chd := halfFloor(l)
   for i, j := 0, chd; i < len(txs); i, j = i + 1, j + 1 {
-    mt[j] = txs[i]
+    merkleTree[j] = txs[i]
   }
   l, par := chd + len(txs), halfFloor(chd)
   for chd > 0 {
     for i, j := chd, par; i < l; i, j = i + 2, j + 1 {
-      if mt[i + 1] != 0 {
-        mt[j], _ = strconv.Atoi(fmt.Sprintf("%d%d", mt[i], mt[i + 1]))
-      } else {
-        mt[j] = mt[i]
-      }
+      merkleTree[j] = pairHash(merkleTree[i], merkleTree[i + 1])
     }
     chd = halfFloor(chd)
     l, par = chd * 2, halfFloor(chd)
   }
-  fmt.Println(mt)
-  return mt, nil
+  fmt.Println(merkleTree)
+  return merkleTree, nil
+}
+
+func MerkleProve(tx int, merkeTree []int) ([]int, error) {
+  i := slices.Index(merkeTree, tx)
+  if i == -1 {
+    return nil, fmt.Errorf("merkle prove: transaction %v not found", tx)
+  }
+  parentRight := func(i int) int {
+    return (i - 1) / 2 + 1
+  }
+  parentLeft := func(i int) int {
+    return (i - 2) / 2 - 1
+  }
+  merkleProof := make([]int, 0)
+  if i % 2 == 0 {
+    i--
+    merkleProof = append(merkleProof, merkeTree[i])
+    merkleProof = append(merkleProof, merkeTree[i + 1])
+  } else {
+    merkleProof = append(merkleProof, merkeTree[i])
+    merkleProof = append(merkleProof, merkeTree[i + 1])
+    i++
+  }
+  for {
+    if i % 2 == 0 {
+      i = parentLeft(i)
+    } else {
+      i = parentRight(i)
+    }
+    merkleProof = append(merkleProof, merkeTree[i])
+    if i == 2 || i == 1 {
+      break
+    }
+  }
+  fmt.Println(merkleProof)
+  return merkleProof, nil
+}
+
+func MerkleVerify(tx int, merkleProof []int, merkleRoot int) bool {
+  return true
 }
--- chain/merkle_test.go
+++ chain/merkle_test.go
@@ -1,20 +1,34 @@
 package chain_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/volodymyrprokopyuk/go-blockchain/chain"
 )
 
 func TestMerkleHash(t *testing.T) {
-  chain.MerkleHash([]int{1, 2, 3, 4, 5, 6, 7, 8})
-  chain.MerkleHash([]int{1, 2, 3, 4, 5, 6, 7})
-  chain.MerkleHash([]int{1, 2, 3, 4, 5, 6})
-  chain.MerkleHash([]int{1, 2, 3, 4, 5})
-  chain.MerkleHash([]int{1, 2, 3, 4})
-  chain.MerkleHash([]int{1, 2, 3})
-  chain.MerkleHash([]int{1, 2})
-  chain.MerkleHash([]int{1})
-  chain.MerkleHash([]int{})
-  chain.MerkleHash([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14})
+  txs := []int{1, 2, 3, 4, 5, 6, 7, 8}
+  // txs := []int{1, 2, 3, 4, 5, 6, 7}
+  // txs := []int{1, 2, 3, 4, 5, 6}
+  // txs := []int{1, 2, 3, 4, 5}
+  // txs := []int{1, 2, 3, 4}
+  // txs := []int{1, 2, 3}
+  // txs := []int{1, 2}
+  // txs := []int{1}
+  // txs := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+  merkleTree, err := chain.MerkleHash(txs)
+  if err != nil {
+    t.Fatal(err)
+  }
+  merkleRoot := merkleTree[0]
+  // for _, tx := range []int{6, 11} {
+  for _, tx := range []int{4} {
+    merkleProof, err := chain.MerkleProve(tx, merkleTree)
+    if err != nil {
+      t.Fatal(err)
+    }
+    valid := chain.MerkleVerify(tx, merkleProof, merkleRoot)
+    fmt.Println(valid)
+  }
 }
