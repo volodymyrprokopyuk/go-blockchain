bb8730da5da21e74833e68894d8a79f6b788ce7e
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-11-09 22:49:34 +0100

    node: add Merkle proof CLI example

--- client.fish
+++ client.fish
@@ -12,24 +12,29 @@ function txSignAndSend -a node from to value ownerpass
   ./bcn tx send --node $node --sigtx $tx
 end
 
-function txProveAndVerify -a node hash mrkroot
-  set mrkproof (./bcn tx prove --node $node --hash $hash)
+function txProveAndVerify -a prover verifier hash mrkroot
+  set mrkproof (./bcn tx prove --node $prover --hash $hash)
   echo MerkleProof $mrkproof
   echo MerkleRoot $mrkroot
-  ./bcn tx verify --node $node --hash $hash \
+  ./bcn tx verify --node $verifier --hash $hash \
     --mrkproof $mrkproof --mrkroot $mrkroot
 end
 
-set acc1 66d614174909403746df7c3222cd74ca386995e4de11cfc99ca1efe548d33105
-set acc2 0a6c57d451f561d6baefe35bba47f8dd682b31da27f0dfdedc646648ea5d12ba
+set acc1 231c83f0a857cfb1e88f8adb92371e01aa1bdc80ef88ea443a2fccf02f444720
+set acc2 cb68e5de26f72110e13e47b2519fcd48ca941a0f4f572bd9751654d01499b910
 
 # txSignAndSend $boot $acc1 $acc2 2 $ownerpass
 # txSignAndSend $boot $acc2 $acc1 1 $ownerpass
 
-set tx1 4312eb8f506a00c4f4f111ea8b318a871615115e5b1a49f14784c5f90a04baeb
-set mrk1 c39f7787a0e1ad825964226031d1ede60f4a8546ce4a5f724321b22ffc3c7394
-set tx2 bd849704122be82ee588c2abfacb8e12fb5bac0916356babcdb2b1683bbc684e
-set mrk2 c39f7787a0e1ad825964226031d1ede60f4a8546ce4a5f724321b22ffc3c7394
+set tx1 6040ff5315af566ed974a737dbf460f04e73c9a713ef494e9baacfe7dd5dc8f1
+set mrk1 6040ff5315af566ed974a737dbf460f04e73c9a713ef494e9baacfe7dd5dc8f1
+set tx2 b87703f6bf0035613f638657293da795bc771ff414c000da894b05d22f5a70b8
+set mrk2 b87703f6bf0035613f638657293da795bc771ff414c000da894b05d22f5a70b8
 
-# txProveAndVerify $boot $tx1 $mrk1
-# txProveAndVerify $boot $tx2 $mrk2
+# txProveAndVerify $boot $boot $tx1 $mrk1
+# txProveAndVerify $boot $boot $tx2 $mrk2
+
+# txProveAndVerify $node $boot $tx1 $mrk1
+# txProveAndVerify $node $boot $tx2 $mrk2
+
+# txProveAndVerify $node $boot $tx1 $mrk2
