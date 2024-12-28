bedc8098a26156edd0a8846de97ab9da360cf624
Volodymyr Prokopyuk <volodymyrprokopyuk@gmail.com>
2024-12-28 21:16:48 +0100

    tx: use original Keccak256 hash function

--- chain/tx.go
+++ chain/tx.go
@@ -14,9 +14,10 @@ type Hash [32]byte
 
 func NewHash(val any) Hash {
   jval, _ := json.Marshal(val)
-  hash := make([]byte, 64)
-  sha3.ShakeSum256(hash, jval)
-  return Hash(hash[:32])
+  state := sha3.NewLegacyKeccak256()
+  _, _ = state.Write(jval)
+  hash := state.Sum(nil)
+  return Hash(hash)
 }
 
 func (h Hash) String() string {
